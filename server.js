2471c8a7429c145e36ef3d081396815c9c2bb2b2
npcpro <britandj7@gmail.com>
2021-02-26 15:57:35 +0700

    delete git ignore dist

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use("/api/item", require('./routers/item'))
 app.use("/api/transaction", require('./routers/transaction'))
 app.use('/public', express.static('public'))
 
-app.use(express.static("./dist"))
+app.use(express.static(__dirname + "./dist"))
 app.get('*', (req, res) => {
-	res.sendFile(path.join("./dist/index.html"))
+	res.sendFile(path.join(__dirname + "./dist/index.html"))
 })
 
 app.get('*', async function (_, res) {
